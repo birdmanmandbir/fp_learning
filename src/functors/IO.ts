e2a17f99b54e44ea6a843aeab92479c15975c762
neil <bn0010100@gmail.com>
2022-09-12 11:05:51 +0800

    add IO functor

---
+++ src/functors/IO.ts
@@ -0,0 +1,68 @@
+import _ from "ramda";
+import { Functor } from "./Functor";
+import { Maybe } from "./Maybe";
+
+type anyFunc = (a: any) => unknown;
+
+export class IO<T extends anyFunc> extends Functor<T> {
+  static of(x: anyFunc) {
+    return new IO(function () {
+      return x;
+    });
+  }
+
+  map<V extends anyFunc>(f: V) {
+    return new IO(_.compose(f, this.__value));
+  }
+}
+
+//  io_window_ :: IO Window
+var io_window = new IO(function () {
+  return window;
+});
+
+io_window.map(function (win: Window) {
+  return win.innerWidth;
+});
+// IO(1430)
+
+io_window.map(_.prop("location")).map(_.prop("href")).map(_.split("/"));
+// IO(["http:", "", "localhost:8000", "blog", "posts"])
+
+//  $ :: String -> IO [DOM]
+var $ = function (selector: any) {
+  return new IO(function () {
+    return document.querySelectorAll(selector);
+  });
+};
+
+$("#myDiv")
+  .map(_.head)
+  .map(function (div) {
+    return div.innerHTML;
+  });
+// IO('I am some inner html')
+
+////// 纯代码库: lib/params.js ///////
+
+//  url :: IO String
+var url = new IO(function() { return window.location.href; });
+
+//  toPairs =  String -> [[String]]
+var toPairs = _.compose(_.map(_.split('=')), _.split('&'));
+
+//  params :: String -> [[String]]
+var params = _.compose(toPairs, _.last, _.split('?'));
+
+const eq = _.curry((a: string, b: string) => a == b);
+
+//  findParam :: String -> IO Maybe [String]
+var findParam = function(key: string) {
+  return _.map(_.compose(Maybe.of, _.filter(_.compose(eq(key), _.head)), params), url);
+};
+
+////// 非纯调用代码: main.js ///////
+
+// 调用 __value() 来运行它！
+findParam("searchTerm").__value();
+// Maybe(['searchTerm', 'wafflehouse'])
